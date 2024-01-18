fe1d7496fbffe6ca9f049805c837528aec7750e1
Kevvsalas <kevin10salas@gmail.com>
2024-01-18 00:42:42 -0600

    delete add button
    Solo funciona TO DO List con enter.key

--- TODO list/script.js
+++ TODO list/script.js
@@ -5,24 +5,6 @@ let list = document.querySelector('.tasks');
 let clear = document.getElementById('noMore');
 let delete_btn = document.getElementById('delete');
 
-
-function add(e){
-    let input = document.getElementById('task');
-    let list = document.querySelector('.tasks');
-    let clear = document.getElementById('noMore');
-    
-    let newtask = document.createElement('p');
-    newtask.textContent = input.value;
-    list.appendChild(newtask);
-    clear.style.display='none';
-    input.value = '';
-    e.preventDefault();
-
-}
-
-
-
-
 function borrar(){
     while(list.children.length >= 1){
         list.removeChild(list.firstChild)
@@ -33,15 +15,16 @@ function borrar(){
 
 input.addEventListener('keydown',(e) =>{
     if (e.key === 'Enter'){
-        let newtask = document.createElement('p');
+        let newtask = document.createElement('div');
         newtask.className='newtask';
+        let btn_delete = document.createElement('button')
         newtask.textContent = input.value;
         list.appendChild(newtask);
+
         clear.style.display='none';
         input.value = '';
 
 
         e.preventDefault();
-        
         }
     });
