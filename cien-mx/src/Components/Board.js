bf05363ef3951698f35317577fc21b06ab6b24c0
Kevvsalas <kevin10salas@gmail.com>
2024-01-21 20:15:05 -0600

    Clean default react project
    Proyecto listo para ser iniciado

---
+++ cien-mx/src/Components/Board.js
@@ -0,0 +1,17 @@
+import './estilos/Board.css'
+function Board() {
+    return(
+        <div className='Board'>
+            <ul>
+                <li>Respuesta..........45</li>
+                <li>Respuesta..........45</li>
+                <li>Respuesta..........45</li>
+                <li>Respuesta..........45</li>
+                <li>Respuesta..........45</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Board;
+ 
---
+++ cien-mx/src/Components/Score.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './estilos/Score.css'
+
+
+
+function Score() {
+    return(
+        <div className='ScoreBoard'>
+            
+        </div>
+    );
+};
+
+export default Score;
